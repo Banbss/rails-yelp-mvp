a4d50d4b3c1fe9f541e43cc58d7a21cb685a9fda
Alban de Morel <albandemorel@hotmail.fr>
2023-05-24 16:31:25 +0200

    yelp almost finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+tour_dargent = { name: "La Tour d'Argent", address: "Tour Eiffel, Paname", category: "french" }
+emporietto = { name: "Emporietto", address: "Nantes", category: "italian" }
+leon = { name: "Leon de Bruxelles", address: "Bruxelles", category: "belgian" }
+delices = { name: "Aux delices d'Asie", address: "every city in the world", category: "chinese" }
+kebab = { name: "Maitre Kebabier", address: "a cote du 10 passage de la poule noire", category: "french" }
+
+
+[tour_dargent, emporietto, leon, delices, kebab].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
